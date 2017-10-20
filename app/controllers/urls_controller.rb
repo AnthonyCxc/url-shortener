ce1420295c0720b3aaed222e78ce7e6b895d605e
AnthonyCxc <AnthonyCxc95@gmail.com>
2017-10-20 14:50:59 +0800

    Shorten_url working

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -7,4 +7,26 @@ class UrlsController < ApplicationController
 	def show
    @urls = Url.find_by(params[:id])
 	end
+
+	def new
+    @urls = Url.new
+	end
+
+	def create
+		# byebug
+    @urls = Url.new(url_params)
+    @urls.shorten
+  if @urls.save
+      redirect_to @url  
+  else
+      render 'new'
+      # ActiveModel::ForbiddenAttributesError means require strong params
+  end   
+end
+
+private 
+def url_params
+    params.require(:url).permit(:long_url)
+end
+
 end
